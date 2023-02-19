7984dea2310d72fe4849e6da6096a63700d6696e
PrSmitch <fivers2b@gmail.com>
2023-02-19 20:07:09 +0300

    READY1

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,13 @@ import (
 	"log"
 
 	"github.com/PrSmitch/first-app"
+	handler "github.com/PrSmitch/first-app/pkg/handlers"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(first.Server)
-	if err := srv.Run("8888"); err != nil {
+	if err := srv.Run("8888", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 
 	}
---
+++ user.go
@@ -0,0 +1,8 @@
+package first
+
+type User struct {
+	Id       int    `json: "-"`
+	Name     string `json: "name"`
+	Username string `json: "username"`
+	Password string `json: "password"`
+}
